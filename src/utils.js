79a25dae2d3b5f375c96966c4f79544349bb2d26
ferryal <ferryal.bahari@amartha.com>
2021-04-29 16:01:52 +0700

    feat: integrate get repo issues

---
+++ src/utils.js
@@ -0,0 +1,28 @@
+const headers = {
+  'Content-Type': 'application/json',
+}
+
+export const getRepoDetails = async () => {
+  try {
+    const res = await fetch(`https://api.github.com/repos/angular/angular`, headers);
+    if (res.status === 200) {
+      return await res.json();
+    }
+    throw Error(res.message);
+  } catch (error) {
+    throw Error(error);
+  }
+}
+
+export const getRepoIssues = async ({ sizePerPage, page}, query) => {
+  try {
+    const res = await fetch(`https://api.github.com/search/issues?q=repo:angular/angular${(query ? '/' + query : '' )}&per_page=${sizePerPage}&page=${page}`, headers);
+    if (res.status === 200) {
+      const data = await res.json();
+      return  {...data, per_page: sizePerPage, page: page, query};
+    }
+    throw Error(res.message);
+  } catch (error) {
+    throw Error(error);
+  }
+}
